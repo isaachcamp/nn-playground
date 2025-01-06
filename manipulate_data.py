624ff1dd9e69521ccbf4687436a3da03523a7406
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-01-06 12:56:45 +0000

    Add utility funcs for data manipulation

---
+++ manipulate_data.py
@@ -0,0 +1,10 @@
+
+import jax.numpy as jnp
+
+def one_hot(x, num_classes):
+    """Create a one-hot encoding of x."""
+    return jnp.array(x[:, None] == jnp.arange(num_classes), jnp.float32)
+
+def flatten(x):
+    """Flatten a multi-dimensional array x."""
+    return x.reshape(x.shape[0], -1)
