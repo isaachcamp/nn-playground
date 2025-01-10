a53650228c96e8932e281adcb18325087c502a05
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-01-10 14:23:07 +0000

    Simple neural network

--- nn.py
+++ nn.py
@@ -1,100 +1,165 @@
 
-from jax import value_and_grad
+from functools import partial
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
 import jax.numpy as jnp
+from jax import grad, jit, vmap
+from jax import random
+from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
 
 from layers import Dense
-from regularisation import Dropout
-from functional import relu_forward, softmax, categorical_cross_entropy_loss
-from optimisers import exponential_decay
 from manipulate_data import one_hot, flatten
+from metrics import accuracy
+import functional as F
+from optimisers import Optimiser, Adam
+from regularisation import Dropout
 
 
-LEARNING_RATE = 0.01
 EPOCHS = 50
+BATCH_SIZE = 128
+LEARNING_RATE = 5e-4
+DROPOUT = 0.05
 
+
+class FlattenAndCast(object):
+    """Data transform to flatten images and cast as type JAX float32."""
+    def __call__(self, pic):
+        return np.ravel(np.array(pic, dtype=jnp.float32))
 
 class NeuralNetwork:
-    def __init__(self, layers, params, training: bool = True) -> None:
+    def __init__(
+            self,
+            layers,
+            params,
+            optimiser: Optimiser,
+            dropout: Dropout = Dropout(DROPOUT),
+            training: bool = True
+        ) -> None:
+
         self.layers = layers
         self.set_params(params)
+        self.optimiser = optimiser
+        self.dropout = dropout
         self.training = training
 
     def set_params(self, params):
         self.params = params
+    
+    def predict(self, params, x):
+        activations = x
+        for p in params[:-1]:
+            outputs = F.linear_forward(activations, p['weights'], p['biases'])
 
-    def forward(self, params, x, dropout=Dropout(0.2)):
-        for layer, p in zip(self.layers[:-1], params[:-1]):
-            x = layer.forward(x, p['weights'], p['biases'])
+            # outputs = self.dropout(outputs) if self.training else outputs
 
-            if isinstance(layer, Dense) and self.training:
-                x = dropout(x)
+            activations = F.relu(outputs)
 
-        x = self.layers[-1].forward(x, params[-1]['weights'], params[-1]['biases'])
-        return x
+        final_p = params[-1]
+        logits = F.linear_forward(activations, final_p['weights'], final_p['biases'])
+        return F.logsoftmax(logits)
 
     def __call__(self, params, x):
-        return self.forward(params, x)
+        return self.predict(params, x)
 
-    def update(self, params, grads, lr):
-        for p, g in zip(params, grads):
-            p['weights'] -= lr * g['weights']
-            p['biases'] -= lr * g['biases']
-        self.set_params(params)
+# Lambda fn, Batched version of `nn.predict`
+batched_predict = vmap(NeuralNetwork.__call__, in_axes=(None, None, 0))
 
+def loss(params, model, inputs, targets):
+    y_pred = batched_predict(model, params, inputs) # Batched version of `nn.predict`
+    return F.categorical_cross_entropy(targets, y_pred)
 
-def init_params(layers):
+@partial(jit, static_argnames=['model'])
+def update(params, x, y, model):
+    grads = grad(loss)(params, model, x, y)
+    return model.optimiser.update_params(params, grads)
+
+def init_params(layers, key):
     """Initialise parameters for all layers in the network."""
     # Collect parameters for JAX grad computation.
-    return [layer.init_params() for layer in layers]
-
-def loss(params, model, x, y_true):
-    y_pred = model(params, x)
-    return categorical_cross_entropy_loss(y_true, y_pred)
-
+    keys = random.split(key, len(layers))
+    return [layer.init_params(key) for key,layer in zip(keys, layers)]
 
 if __name__ == '__main__':
-
-    # Load MNIST dataset.
-    train_dataset = datasets.MNIST('./data', train=True,   download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-    ]))
-
-    test_dataset = datasets.MNIST('./data', train=False, download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ]))
-
-    train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True)
-
-    # Initialize neural network parameters.
+    
     # Set up neural network.
+    n_classes = 10
+    input_size = 28 * 28
+
     layers = [
-            Dense((784, 512), activation=relu_forward),
-            Dense((512, 512), activation=relu_forward),
-            Dense((512, 24), activation=relu_forward),
-            Dense((24, 10), activation=softmax)
+            Dense((input_size, 512)),
+            Dense((512, 512)),
+            Dense((512, n_classes)),
         ]
-    params = init_params(layers)
-    nn = NeuralNetwork(layers, params)
+    params = init_params(layers, random.key(0))
+    optimiser = Adam(LEARNING_RATE)
+    nn = NeuralNetwork(layers, params, optimiser)
 
-    # Train the neural network.
+    # Load data and create batched generator `dataloader`.
+    train_dataset = MNIST('./data', train=True, download=True, transform=FlattenAndCast())
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    # Get full test and train datasets (for checking accuracy)
+    train_images = flatten(jnp.array(train_dataset.train_data))
+    train_labels = one_hot(train_dataset.train_labels.numpy(), n_classes)
+
+    test_dataset = MNIST('./data', train=False, download=True)
+    test_images = flatten(jnp.array(test_dataset.test_data.numpy()))
+    test_labels = one_hot(test_dataset.test_labels.numpy(), n_classes)
+
+    # Training loop
+    train_accs = []
+    test_accs = []
     costs = []
+
     for epoch in range(EPOCHS):
-        for train_data, train_labels in iter(train_dataloader):
-            inputs = flatten(jnp.array(train_data.numpy()))
-            targets = jnp.array(one_hot(train_labels.numpy(), num_classes=10))
+        start_time = time.time()
+        for x, y in train_dataloader:
+            inputs = flatten(jnp.array(x.numpy()))
+            targets = one_hot(y.numpy(), n_classes)
 
-            cost, grads = value_and_grad(loss)(nn.params, nn, inputs, targets)
+            # Backpropagate gradients and update parameters.
+            params, optimiser_state = update(params, inputs, targets, nn)
+            nn.set_params(params)
+            nn.optimiser.set_state(optimiser_state) # Removes side effects from `update`.
 
-            learning_rate = exponential_decay(LEARNING_RATE, step=epoch)
-            nn.update(nn.params, grads, learning_rate)
+        # Evaluation
+        epoch_time = time.time() - start_time
 
+        nn.training = False # Disable dropout for evaluation
+        train_acc = accuracy(train_labels, y_pred=batched_predict(nn, params, train_images))
+        test_acc = accuracy(test_labels, y_pred=batched_predict(nn, params, test_images))
+        cost = loss(params, nn, train_images, train_labels)
+        nn.training = True
+
+        print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
+        print("Training set accuracy {}".format(train_acc))
+        print("Test set accuracy {}".format(test_acc))
+
+        train_accs.append(train_acc)
+        test_accs.append(test_acc)
         costs.append(cost)
 
-        if epoch % 10 == 0:
-            print(f'Epoch {epoch}: Loss = {cost}')
+        optimiser.next_epoch()
+
+    # Accuracy plot
+    x = np.arange(len(train_accs))
+
+    plt.plot(x, train_accs, label='Train')
+    plt.plot(x, test_accs, label='Test', linestyle='--')
+    ax = plt.gca()
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Cross-entropy Cost')
+    ax.legend()
+    plt.savefig('test_train_accuracy_nn.png')
+    plt.close()
+
+    # Loss plot
+    plt.plot(np.arange(len(costs)), costs)
+    ax = plt.gca()
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Cross-entropy Cost')
+    plt.savefig('cost_nn.png')
+    plt.close()
