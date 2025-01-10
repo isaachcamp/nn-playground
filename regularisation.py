6385a624a0ea88ba99378bd4582f42d9589241f9
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-01-10 14:40:57 +0000

    Add L2 regulariser

--- regularisation.py
+++ regularisation.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import jax.numpy as jnp
 
 class Dropout:
     def __init__(self, p: float):
@@ -11,3 +12,13 @@ class Dropout:
         mask = np.random.rand(*x.shape) > self.p
         scaled_mask = mask / (1 - self.p) # Scale values to maintain expected value
         return x * scaled_mask
+
+class L2Regularisation:
+    def __init__(self, lambda_: float):
+        """Initialize L2 Regularisation layer."""
+        self.lambda_ = lambda_
+
+    def __call__(self, params):
+        """Compute L2 Regularisation loss."""
+        sum_of_squares = [jnp.sum(jnp.square(layer['weights'])) for layer in params]
+        return (self.lambda_ / 2) * sum(sum_of_squares)
