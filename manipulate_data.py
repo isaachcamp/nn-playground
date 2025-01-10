fedc2beb04f0029745508411876429f5170bb5c7
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-01-10 14:03:13 +0000

    update docstring on one_hot

--- manipulate_data.py
+++ manipulate_data.py
@@ -2,7 +2,7 @@
 import jax.numpy as jnp
 
 def one_hot(x, num_classes):
-    """Create a one-hot encoding of x."""
+    """Create a one-hot encoding of x for n classes."""
     return jnp.array(x[:, None] == jnp.arange(num_classes), jnp.float32)
 
 def flatten(x):
