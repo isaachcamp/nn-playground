24f579defb16ff2edb532b358e7a8fe79052d082
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-01-10 14:07:36 +0000

    initialise weights as transpose Remove unnecessary initialise_weights fn Add jax keys for consistent initialisation

--- initialisations.py
+++ initialisations.py
@@ -1,43 +1,17 @@
 
-import jax
+from jax import random
 import jax.numpy as jnp
 
 Array = jnp.ndarray
 
-def random_init_weights(dims: tuple, key: Array) -> Array:
-    """Generate random weights from Normal distribution and scale by 0.01."""
-    return jax.random.normal(key, dims) * 0.01
+def random_init_weights(dims: tuple, key: Array, scale: float = 1e-2) -> Array:
+    """Generate random weights from Normal distribution and scale."""
+     # initialise as W^transpose to remove need for later transpositions.
+    return scale * random.normal(key, (dims[-1], *dims[:-1]))
 
 def he_init_weights(dims: tuple, key: Array) -> Array:
     """Initialise weights using He (2015) method, with normalised variance."""
     # Normalisation depends on size of previous layer
     prev_layer_size = jnp.array(dims[:-1]).prod()
-    return jax.random.normal(key, dims) * jnp.sqrt(2 / prev_layer_size)
-
-def initialise_weights(
-        dims: tuple,
-        method: str = 'default',
-        seed: int = 42
-    ) -> Array:
-    """
-    Initialise weights for a given layer.
-
-    Parameters:
-    method (str): The method to use for weight initialization. Options are 
-        "dflt" for default random initialization and "He" for He initialization.
-    dims (tuple): The dimensions of the weights to be initialized.
-    seed (int, optional): The seed for the random number generator. Defaults 
-        to None.
-
-    Returns:
-    np.ndarray: The initialized weights as a NumPy array.
-    """
-
-    key = jax.random.key(seed)
-
-    if method == 'default':
-        return random_init_weights(dims, key)
-    elif method == 'He':
-        return he_init_weights(dims, key)
-
-    raise KeyError(f'Method "{method}" not recognised.')
+     # initialise as W^transpose to remove need for later transpositions.
+    return random.normal(key, (dims[-1], *dims[:-1])) * jnp.sqrt(2 / prev_layer_size)
