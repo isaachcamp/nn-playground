27d16053c8686cdd86eab70fa6dd8cbb21cb343d
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-01-10 14:08:05 +0000

    rename drop_prob to p

--- regularisation.py
+++ regularisation.py
@@ -2,13 +2,12 @@
 import numpy as np
 
 class Dropout:
-    def __init__(self, drop_prob: float):
+    def __init__(self, p: float):
         """Initialize Dropout layer."""
-        self.drop_prob = drop_prob
+        self.p = p
 
     def __call__(self, x):
-        """Apply dropout during forward pass."""
-        # Mask input values with probability drop_prob
-        mask = np.random.rand(*x.shape) > self.drop_prob
-        scaled_mask = mask / (1 - self.drop_prob) # Scale values to maintain expected value
+        """Set some nodes to zero with some probability `p` during forward pass."""
+        mask = np.random.rand(*x.shape) > self.p
+        scaled_mask = mask / (1 - self.p) # Scale values to maintain expected value
         return x * scaled_mask
